367cc6f1eae42e07b75d7b3abafd8a89f889b5dc
alexis39tt <alessandro.fonzi@gmail.com>
2022-05-12 20:04:14 +0200

    added traits

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php
 
-$date_month = date('m');
-$date_year = date('y');
+trait Dates{
+    public $date_month = date('m');
+    public $date_year = date('y');
+}
 
 class Product{
     public $product_name;
     public $product_id;
     public $product_price;
+    public $product_category;
     public $product_available = true;
+    public function setName($product_name){
+        $this->product_name = $product_name;
+    }
     public function UserDiscount($user_discount){
         if($user_discount > 0)
             $this->product_price = $this->product_price*((100-$user_discount)/100);
@@ -15,6 +21,7 @@ class Product{
 }
 
 class SeasonalProduct extends Product{
+    use Dates;
     public $available_from = 5;
     public $available_to = 8;
     public function isProductAvailable($date_month){
@@ -24,9 +31,12 @@ class SeasonalProduct extends Product{
             $this->product_available = false;
     }
 }
-
+class Toys extends Product{
+    public $product_category = 'Toy';
+}
 class User
-{
+{   
+    use Dates;
     public $user_discount = 0;
     public $cc_month;
     public $cc_year;
